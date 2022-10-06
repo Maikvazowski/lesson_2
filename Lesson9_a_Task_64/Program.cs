395b406c04174fa69e2e8084b4d6f601a6b90d25
Ilya <maikvazowski@bk.ru>
2022-10-06 22:46:20 +0500

    Lesson9_A_Task_64

---
+++ Lesson9_a_Task_64/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все чётные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+//N = 5 -> "4, 2"
+//N = 8 -> "8, 6, 4, 2"
+
+int n = InputInt("Введите положительное число ");
+int m = 1;
+if (n < 1)
+{
+    Console.WriteLine("Ввели не положительное число");
+}
+Console.WriteLine(NaturalNumber(n, m));
+
+int NaturalNumber(int n, int m)
+{
+    if (n == m)
+        return n;
+    else
+        Console.Write($"{NaturalNumber(n, m + 1)}, ");
+    return m;
+}
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
